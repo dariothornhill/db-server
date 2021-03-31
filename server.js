3667f4500c481a122037473e11efc3e4b60fec16
Dario Thornhill <dario.thornhill@gmail.com>
2021-03-31 14:02:42 +0300

    Updated server code

--- server.js
+++ server.js
@@ -7,15 +7,22 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
 const DATABASE_URL = process.env.DATABASE_URL;
+const NODE_ENV = process.env.NODE_ENV;
 
 // Load other libraries
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
+
+// Loading the pg client library
 const pg = require('pg');
 
-const client = new pg.Client(DATABASE_URL);
-client.on('error', err => { throw err; });
+// Setup our connection options based on environment
+const options = NODE_ENV === 'production' ? { connectionString: DATABASE_URL, ssl: { rejectUnauthorized: false } } : { connectionString: DATABASE_URL };
+
+// Creating a postgres database client with supplied options
+const client = new pg.Client(options);
+client.on('error', err => { console.log('Unable to connect to database'); });
 
 // app setup
 const app = express();
@@ -23,38 +30,45 @@ app.use(cors());
 
 app.get('/location', handle);
 
-// This will only start out webserver if we can successfully connect to the database
-client.connect().then(() => {
-    app.listen(PORT, () => {
-        console.log(`App listening on port ${PORT}`)
-    })
-});
+// Connect to the database
+client.connect()
+    .then(() => {
+        // This will only start out webserver if we connected successfully
+        app.listen(PORT, () => {
+            console.log(`App listening on port ${PORT}`);
+        })
+    }).catch(error => {
+        console.log('Unable to connect to database: ', error.message);
+    });
 
 // /location?city=Amman
 function handle(req, res) {
     const city = req.query.city;
-    const select = "SELECT * FROM locations WHERE city = $1";
+    const sql = "SELECT * FROM locations WHERE city = $1";
+    const sqlArray = [city];
 
-    client.query(select, [city]).then((data) => {
-        console.log(data.rows);
-        if (data.rowCount === 0) {
-            const url = `https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&city=${city}&format=json`;
-            superagent.get(url).then(data => {
-                // TODO use constructor to format this data
-                const locationData = data.body[0];
-                const newLocation = new Location(city, locationData.display_name);
-                // Save it to a table in our database
-                const sql = 'INSERT INTO locations (city, display_name) VALUES ($1, $2) RETURNING *';
-                const cleanValues = [newLocation.city, newLocation.display_name];
-                //respond to client
-                client.query(sql, cleanValues).then((data) => {
-                    res.json(data.rows[0]);
+    client.query(sql, sqlArray)
+        .then((data) => {
+            if (data.rowCount === 0) {
+                const url = `https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&city=${city}&format=json`;
+                superagent.get(url).then(data => {
+                    // TODO use constructor to format this data
+                    const locationData = data.body[0];
+                    const newLocation = new Location(city, locationData.display_name);
+                    // Save it to a table in our database
+                    const sql = 'INSERT INTO locations (city, display_name) VALUES ($1, $2) RETURNING *;';
+                    const cleanValues = [newLocation.city, newLocation.display_name];
+                    //respond to client
+                    client.query(sql, cleanValues)
+                        .then((data) => {
+                            console.log(data);
+                            res.json(data.rows[0]);
+                        });
                 })
-            })
-        } else {
-            res.json(data.rows[0]);
-        }
-    });
+            } else {
+                res.json(data.rows[0]);
+            }
+        });
 }
 
 function Location(city, display_name) {
